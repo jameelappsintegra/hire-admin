d7172da2fbae2b205cc9db46f0a659f25cb636c0
jameelappsint <mail4jameelmca@gmail.com>
2021-01-05 14:00:31 +0400

    job listing changes

--- src/app/view/job-openings/job-openings-list/job-openings-list.component.ts
+++ src/app/view/job-openings/job-openings-list/job-openings-list.component.ts
@@ -7,15 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class JobOpeningsListComponent implements OnInit {
   currentOpenings = [
-    { jobName: 'Web developer', location: 'Confidential Company' },
-    { jobName: 'Executive Quality Product', location: 'Confidential Company' },
-    { jobName: 'Representative Solutions Central', location: 'Confidential Company' },
-    { jobName: 'Specialist Security National', location: 'Confidential Company' },
-    { jobName: 'Technician Assurance Internal', location: 'Confidential Company' },
-    { jobName: 'Consultant Integration Chief', location: 'Confidential Company' },
-    { jobName: 'Analyst Intranet Regional', location: 'Confidential Company' },
-    { jobName: 'Developer Solutions Regional', location: 'Confidential Company' },
-    { jobName: 'Specialist Division Future', location: 'Confidential Company' }]
+    { jobName: 'Web developer', location: 'Confidential Company', endDate: '10 Dec 2020' },
+    { jobName: 'Executive Quality Product', location: 'Confidential Company', endDate: '20 Dec 2020' },
+    { jobName: 'Representative Solutions Central', location: 'Confidential Company', endDate: '10 Sep 2020' },
+    { jobName: 'Specialist Security National', location: 'Confidential Company', endDate: '15 Dec 2020' },
+    { jobName: 'Technician Assurance Internal', location: 'Confidential Company', endDate: '10 Jan 2020' },
+    { jobName: 'Consultant Integration Chief', location: 'Confidential Company', endDate: '19 Dec 2020' },
+    { jobName: 'Analyst Intranet Regional', location: 'Confidential Company', endDate: '21 July 2020' },
+    { jobName: 'Developer Solutions Regional', location: 'Confidential Company', endDate: '25 Dec 2020' },
+    { jobName: 'Specialist Division Future', location: 'Confidential Company', endDate: '10 Dec 2020' }]
 
   constructor() { }
 
