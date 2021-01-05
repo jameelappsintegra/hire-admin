537c9ab3e971a619d63905d6f23eb8a2017236c9
safia <safia@paraminfo.com>
2021-01-05 11:57:18 +0400

    added job create

---
+++ src/app/view/job-openings/job-openings-create/find-candidate/find-candidate.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FindCandidateComponent } from './find-candidate.component';
+
+describe('FindCandidateComponent', () => {
+  let component: FindCandidateComponent;
+  let fixture: ComponentFixture<FindCandidateComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FindCandidateComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FindCandidateComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/view/job-openings/job-openings-create/the-job/the-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-the-job',
+  templateUrl: './the-job.component.html',
+  styleUrls: ['./the-job.component.scss']
+})
+export class TheJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
